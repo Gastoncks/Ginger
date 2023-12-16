2c4af51b586f922983cda1c3cc6a1862932e7b26
Gastoncks <gastonvelperret@gmail.com>
2023-12-16 12:13:41 +0100

    ligne oublier

--- src/main/java/fr/gastoncks/ginger/GingerClient.java
+++ src/main/java/fr/gastoncks/ginger/GingerClient.java
@@ -1,10 +1,13 @@
 package fr.gastoncks.ginger;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import fr.gastoncks.ginger.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class GingerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.GINGER_CROP, RenderLayer.getCutout());
     }
 }
