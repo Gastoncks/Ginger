0971789a4abb87dfacca3adb5de4d093ed3e5e6f
Gastoncks <gastonvelperret@gmail.com>
2023-12-16 12:34:19 +0100

    Datagen fini normalement cette fois

--- src/main/java/fr/gastoncks/ginger/GingerDataGenerator.java
+++ src/main/java/fr/gastoncks/ginger/GingerDataGenerator.java
@@ -1,11 +1,18 @@
 package fr.gastoncks.ginger;
 
+import fr.gastoncks.ginger.datagen.ModBlockTagProvider;
+import fr.gastoncks.ginger.datagen.ModLootTableProvider;
+import fr.gastoncks.ginger.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class GingerDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
 	}
 }
