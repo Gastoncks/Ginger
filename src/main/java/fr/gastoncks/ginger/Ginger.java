34307d3e5143092533f35b3f33119c9782024047
Gastoncks <gastonvelperret@gmail.com>
2023-12-24 19:56:50 +0100

    modif food effect

--- src/main/java/fr/gastoncks/ginger/Ginger.java
+++ src/main/java/fr/gastoncks/ginger/Ginger.java
@@ -5,12 +5,8 @@ import fr.gastoncks.ginger.item.ModItemGroups;
 import fr.gastoncks.ginger.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Ginger implements ModInitializer {
 	public static final String MOD_ID = "ginger";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +14,6 @@ public class Ginger implements ModInitializer {
         ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroups();
+
 	}
 }
--- src/main/java/fr/gastoncks/ginger/item/ModFoodComponents.java
+++ src/main/java/fr/gastoncks/ginger/item/ModFoodComponents.java
@@ -6,11 +6,11 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent GINGER = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 2), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 1, false,false),1f)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1),0.75f)
             .build();
     public static final FoodComponent CANDIED_GINGER = new FoodComponent.Builder().hunger(4).saturationModifier(0.8f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1000, 2), 1f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 1200, 1, false,false), 1f)
             .statusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 1),1f)
             .build();
 }
