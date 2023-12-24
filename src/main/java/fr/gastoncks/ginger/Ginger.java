9c803193da00c791915f7a183aeb003fb948a2c7
Gastoncks <gastonvelperret@gmail.com>
2023-12-24 22:17:07 +0100

    add loottable to creeper drop and other thing

--- src/main/java/fr/gastoncks/ginger/Ginger.java
+++ src/main/java/fr/gastoncks/ginger/Ginger.java
@@ -3,10 +3,14 @@ package fr.gastoncks.ginger;
 import fr.gastoncks.ginger.block.ModBlocks;
 import fr.gastoncks.ginger.item.ModItemGroups;
 import fr.gastoncks.ginger.item.ModItems;
+import fr.gastoncks.ginger.util.ModLootTableModifiers;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Ginger implements ModInitializer {
 	public static final String MOD_ID = "ginger";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -14,6 +18,6 @@ public class Ginger implements ModInitializer {
         ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroups();
-
+		ModLootTableModifiers.modifyLootTables();
 	}
 }
---
+++ src/main/java/fr/gastoncks/ginger/util/ModLootTableModifiers.java
@@ -0,0 +1,31 @@
+package fr.gastoncks.ginger.util;
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import fr.gastoncks.ginger.item.ModItems;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
+
+public class ModLootTableModifiers {
+    private static final Identifier CREEPER_ID =
+            new Identifier("minecraft", "entities/creeper");
+
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if(CREEPER_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.1f))
+                        .with(ItemEntry.builder(ModItems.GINGER))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+
+        });
+    }
+}
